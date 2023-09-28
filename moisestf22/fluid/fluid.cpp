0fed0754068b0ae0ddaf270ecd67be86b638eab0
Moisés Jiménez Martínez <moisesjmnez@gmail.com>
2023-09-28 11:29:38 +0200

    Intial changes

---
+++ moisestf22/fluid/fluid.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        std::cerr << "Error: Invalid number of arguments: " << argc-1 << ".\n";
+        return 1;
+    }
+
+    int nts;
+    std::string inputfile, outputfile;
+
+    return 0;
+}
