ab38344d54f0d0415e6baf3b9d0eb06e1b70a4b9
Ron Kibel <ron.kibel1@gmail.com>
2023-10-21 23:41:15 +0200

    modified utility to support hpp

--- sim/utility.cpp
+++ sim/utility.cpp
@@ -1,3 +1,5 @@
+#include "utility.hpp"
+
 #include <iostream>
 #include <fstream>
 
---
+++ sim/utility.hpp
@@ -0,0 +1,8 @@
+#ifndef E227D684_5658_44BF_99B0_34A6508D898B
+#define E227D684_5658_44BF_99B0_34A6508D898B
+
+#include <string>
+
+void binaryToText(std::string inputFile, std::string outputFile);
+
+#endif /* E227D684_5658_44BF_99B0_34A6508D898B */
