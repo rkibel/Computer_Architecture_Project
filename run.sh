fe473d39de37116aa25d087f4d402e49fee420af
RON KIBEL <a0520290@avignon-frontend.lab.inf.uc3m.es>
2023-11-01 15:57:50 +0100

    works for me

--- run.sh
+++ run.sh
@@ -4,4 +4,9 @@
 module avail
 module load gcc/12.1.0
 echo "* Running fluid *"
-perf stat ./build/fluid/fluid 1 small.fld small-1-test.fld
+
+step_count=1
+input_file="small.fld"
+output_file="final.fld"
+
+perf stat ./build/fluid/fluid $step_count $input_file $output_file
