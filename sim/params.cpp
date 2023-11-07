332a4f506ee004d7ca26d801be341602ff9731e0
RON KIBEL <a0520290@avignon-frontend.lab.inf.uc3m.es>
2023-11-07 21:32:41 +0100

    rk - may or may not work correctly

--- sim/params.cpp
+++ sim/params.cpp
@@ -2,37 +2,47 @@
 
 void params::initialize(std::istream & fileReader) {
   int const exit_status_error = -5;                      // Magic numbers
-  ppm                         = read_float(fileReader);  // Reading
-  int const temp_np           = read_int(fileReader);
+  ppm = read_float(fileReader);  // Reading
+  int const temp_np = read_int(fileReader);
   if (temp_np <= 0) {
     std::cerr << "Error: Invalid number of particles: " << temp_np << ".\n";
     exit(exit_status_error);
   }
-  np               = temp_np;
-  mass             = constants::fluid_density / ppm / ppm / ppm;
+  np = temp_np;
+  mass = constants::fluid_density / ppm / ppm / ppm;
   smoothing_length = constants::radius_mult / ppm;
-  initializeVectors();
+  initializeVectors1();
+  initializeVectors2();
 }
 
-void params::initializeVectors() {
-  int const magic_1    = 6;
-  double const magic_2 = 315.0;
-  double const magic_3 = 64.0;
-  int const magic_4    = 9;
-  double const magic_5 = 45.0;
-  density_factors      = {smoothing_length * smoothing_length, std::pow(smoothing_length, magic_1),
-                          magic_2 * mass / magic_3 / std::numbers::pi /
-                              std::pow(smoothing_length, magic_4)};
-  acceleration_factors = {smoothing_length * smoothing_length,
-                          magic_5 * mass * constants::stiff_pressure / std::numbers::pi /
-                              std::pow(smoothing_length, magic_1) / 2,
-                          magic_5 * mass * constants::viscosity / std::numbers::pi /
-                              std::pow(smoothing_length, magic_1)};
+void params::initializeVectors1() {
+  double const magic_6 = -9.8;
+  double const magic_7 = -0.065;
+  double const magic_8 = -0.08;
+  double const magic_9 = 0.1;
+
+  acceleration = {0.0, magic_6, 0.0};
+  min = {magic_7, magic_8, magic_7};
+  max = {-magic_7, magic_9, -magic_7};
+}
+
+void params::initializeVectors2() {
+  int const mag1    = 6;
+  double const mag2 = 315.0;
+  double const mag3 = 64.0;
+  int const mag4    = 9;
+  double const mag5 = 45.0;
+  
+  density_factors = {smoothing_length * smoothing_length, std::pow(smoothing_length, mag1), mag2 
+    * mass / mag3 / std::numbers::pi / std::pow(smoothing_length, mag4)};
+  acceleration_factors = {smoothing_length * smoothing_length, mag5 * mass * 
+    constants::stiff_pressure / std::numbers::pi / std::pow(smoothing_length, mag1) / 2,
+    mag5 * mass * constants::viscosity / std::numbers::pi / std::pow(smoothing_length, mag1)};
   grid_size            = {
-    static_cast<int>(std::floor((constants::max[0] - constants::min[0]) / smoothing_length)),
-    static_cast<int>(std::floor((constants::max[1] - constants::min[1]) / smoothing_length)),
-    static_cast<int>(std::floor((constants::max[2] - constants::min[2]) / smoothing_length))};
-  block_size = {(constants::max[0] - constants::min[0]) / grid_size[0],
-                (constants::max[1] - constants::min[1]) / grid_size[1],
-                (constants::max[2] - constants::min[2]) / grid_size[2]};
+    static_cast<int>(std::floor((max[0] - min[0]) / smoothing_length)),
+    static_cast<int>(std::floor((max[1] - min[1]) / smoothing_length)),
+    static_cast<int>(std::floor((max[2] - min[2]) / smoothing_length))};
+  block_size = {(max[0] - min[0]) / grid_size[0],
+                (max[1] - min[1]) / grid_size[1],
+                (max[2] - min[2]) / grid_size[2]};
 }
