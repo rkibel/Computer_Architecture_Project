7313fe9fec4fb7fcb1c88c06d9b64164636d1269
Ron Kibel <ron.kibel1@gmail.com>
2023-11-18 19:25:24 +0100

    rk - modified all utests: created grid test, and edited other two utests

--- utest/grid_test.cpp
+++ utest/grid_test.cpp
@@ -1,42 +1,137 @@
-// #include "../sim/grid.hpp"
-// #include "../sim/progargs.hpp"
-// #include "../sim/utility.hpp"
+#include "../sim/grid.hpp"
+#include "../sim/progargs.hpp"
+#include "../sim/utility.hpp"
 
-// #include <cstdio>
-// #include <gtest/gtest.h>
+#include <gtest/gtest.h>
 
-// class GridTest : public testing::Test {
-//   public:
-//     void SetUp() override { }
+template <typename T>
+  requires(std::is_integral_v<T> || std::is_floating_point_v<T>)
+char * as_writable_buffer(T & value) {
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+  return reinterpret_cast<char *>(&value);
+}
 
-// void TearDown() override { }
+long read_long(std::istream & is) {
+  long value{};
+  is.read(as_writable_buffer(value), sizeof(value));
+  return value;
+}
 
-// grid small = parseInputFile("reference-input/small.fld");
-// grid large = parseInputFile("reference-input/large.fld");
-// };
+double read_double(std::istream & is) {
+  double value{};
+  is.read(as_writable_buffer(value), sizeof(value));
+  return value;
+}
 
-// TEST_F(BlockTest, TestSmall) {
-//   int const step_count = 5;
-//   auto const read_ppm  = static_cast<float>(small.parameters.ppm);
-//   auto const read_np   = static_cast<int>(small.parameters.np);
-//   for (int i = 0; i < step_count; ++i) {
-//     small.processStep();
-//     std::string const file_name = "small-" + std::to_string(i + 1) + ".fld";
-//     writeFile("ftest/" + file_name, read_ppm, read_np, small.part_dict);
-//     EXPECT_TRUE(compare_binary_files("ftest/" + file_name, "reference-output/" + file_name));
-//     EXPECT_EQ(remove(("ftest/" + file_name).c_str()), 0);
-//   }
-// }
+particle readParticle(std::istream & is, std::vector<int> grid_pos) {
+  particle part;
+  part.id = static_cast<int>(read_long(is));
+  int const three = 3;
+  for (int i = 0; i < three; ++i) { part.position.push_back(read_double(is)); }
+  for (int i = 0; i < three; ++i) { part.boundary.push_back(read_double(is)); }
+  for (int i = 0; i < three; ++i) { part.velocity.push_back(read_double(is)); }
+  part.density = read_double(is);
+  for (int i = 0; i < three; ++i) { part.acceleration.push_back(read_double(is)); }
+  part.grid_pos = grid_pos;
+  return part;
+}
 
-// TEST_F(FTest, TestLarge) {
-//   int const step_count = 5;
-//   auto const read_ppm  = static_cast<float>(large.parameters.ppm);
-//   auto const read_np   = static_cast<int>(large.parameters.np);
-//   for (int i = 0; i < step_count; ++i) {
-//     large.processStep();
-//     std::string const file_name = "large-" + std::to_string(i + 1) + ".fld";
-//     writeFile("ftest/" + file_name, read_ppm, read_np, large.part_dict);
-//     EXPECT_TRUE(compare_binary_files("ftest/" + file_name, "reference-output/" + file_name));
-//     EXPECT_EQ(remove(("ftest/" + file_name).c_str()), 0);
-//   }
-// }
+bool gridEqualsTrz(grid const & grid, std::string const trz) {
+  std::ifstream input;
+  input.open(trz, std::ios::binary);
+  if (!input) { std::cerr << "Error: unable to open trz " << trz << " for reading."; return false; }
+  std::vector<int> gsize = grid.parameters.grid_size;
+  int const num_blocks = read_int(input);
+  for (int i = 0; i < num_blocks; ++i) {
+    std::vector<int> gpos = {i % gsize[0], static_cast<int>(i % (gsize[0] * gsize[1]) / gsize[0]),
+      static_cast<int>(i / (gsize[0] * gsize[1])) };
+    long const num_particles = read_long(input);
+    for (long j = 0; j < num_particles; ++j) {
+      particle const & ref = readParticle(input, gpos);
+      particle const & org = grid.part_dict[ref.id];
+      if (ref != org) {
+        std::cerr << "Error: particles do not match.\nComputed:\n" << org << "Reference:\n" 
+        << ref;
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+// copy of for loop in grid.cpp processStep moved to named function
+void densityIncrement(grid & grid) {
+  for (auto const & surround : grid.grid_neighbor_combinations) {
+    for (size_t i = 0; i < surround.size(); ++i) {
+      grid.updateBlock(surround[0], surround[i], true);
+    }
+  }
+}
+
+// copy of for loop in grid.cpp processStep moved to named function
+void densityTransformation(grid & grid) {
+  for (particle & part : grid.part_dict) {
+    part.density = (part.density + grid.parameters.density_factors[1]) * 
+    grid.parameters.density_factors[2];
+  }
+}
+
+// copy of for loop in grid.cpp processStep moved to named function
+void accelerationTransformation(grid & grid) {
+  for (auto const & surround : grid.grid_neighbor_combinations) {
+    for (size_t i = 0; i < surround.size(); ++i) {
+      grid.updateBlock(surround[0], surround[i], false);
+    }
+  }
+}
+
+class GridTest : public testing::Test {
+  public:
+    void SetUp() override { }
+    void TearDown() override { }
+    int const steps = 5;
+};
+
+TEST_F(GridTest, SmallTest) {
+  grid small = parseInputFile("reference-input/small.fld");
+  for (int i = 1; i < steps + 1; ++i) {
+    std::string const stp = std::to_string(i);
+    small.repositionAndInitialize();
+    EXPECT_TRUE(gridEqualsTrz(small, "trace/small/repos-base-" + stp + ".trz"));
+    EXPECT_TRUE(gridEqualsTrz(small, "trace/small/initacc-base-" + stp + ".trz"));
+    densityIncrement(small);
+    EXPECT_TRUE(gridEqualsTrz(small, "trace/small/densinc-base-" + stp + ".trz"));
+    densityTransformation(small);
+    EXPECT_TRUE(gridEqualsTrz(small, "trace/small/denstransf-base-" + stp + ".trz"));
+    accelerationTransformation(small);
+    EXPECT_TRUE(gridEqualsTrz(small, "trace/small/acctransf-base-" + stp + ".trz"));
+    for (particle & part : small.part_dict) { small.updateAccWithWall(part); }
+    EXPECT_TRUE(gridEqualsTrz(small, "trace/small/partcol-base-" + stp + ".trz"));
+    for (particle & part : small.part_dict) { small.particlesMotion(part); }
+    EXPECT_TRUE(gridEqualsTrz(small, "trace/small/motion-base-" + stp + ".trz"));
+    for (particle & part : small.part_dict) { small.collideWithWall(part); }
+    EXPECT_TRUE(gridEqualsTrz(small, "trace/small/boundint-base-" + stp + ".trz"));
+  }
+}
+
+TEST_F(GridTest, LargeTest) {
+  grid large = parseInputFile("reference-input/large.fld");
+  for (int i = 1; i < steps + 1; ++i) {
+    std::string const stp = std::to_string(i);
+    large.repositionAndInitialize();
+    EXPECT_TRUE(gridEqualsTrz(large, "trace/large/repos-base-" + stp + ".trz"));
+    EXPECT_TRUE(gridEqualsTrz(large, "trace/large/initacc-base-" + stp + ".trz"));
+    densityIncrement(large);
+    EXPECT_TRUE(gridEqualsTrz(large, "trace/large/densinc-base-" + stp + ".trz"));
+    densityTransformation(large);
+    EXPECT_TRUE(gridEqualsTrz(large, "trace/large/denstransf-base-" + stp + ".trz"));
+    accelerationTransformation(large);
+    EXPECT_TRUE(gridEqualsTrz(large, "trace/large/acctransf-base-" + stp + ".trz"));
+    for (particle & part : large.part_dict) { large.updateAccWithWall(part); }
+    EXPECT_TRUE(gridEqualsTrz(large, "trace/large/partcol-base-" + stp + ".trz"));
+    for (particle & part : large.part_dict) { large.particlesMotion(part); }
+    EXPECT_TRUE(gridEqualsTrz(large, "trace/large/motion-base-" + stp + ".trz"));
+    for (particle & part : large.part_dict) { large.collideWithWall(part); }
+    EXPECT_TRUE(gridEqualsTrz(large, "trace/large/boundint-base-" + stp + ".trz"));
+  }
+}
--- utest/progargs_test.cpp
+++ utest/progargs_test.cpp
@@ -1,33 +1,19 @@
 #include "../sim/progargs.hpp"
-#include "../sim/utility.hpp"
-#include "utest_utility.hpp"
-
-#include <cstdio>
 #include <gtest/gtest.h>
 
 class ProgargsTest : public testing::Test {
   public:
     void SetUp() override { }
-
     void TearDown() override { }
-
-    params parameters;
-    params correct_parameters;
-    std::ifstream inFile;
-    std::ofstream outFileTest;
-    std::ofstream outFileCorrect;
 };
 
-TEST_F(ProgargsTest, checkArgNumber_Test) {
+TEST_F(ProgargsTest, checkArgNumberTest) {
   EXPECT_EXIT(checkArgNumber(1), testing::ExitedWithCode(255),
               "Error: Invalid number of arguments: 0.\n");
-
   EXPECT_EXIT(checkArgNumber(3), testing::ExitedWithCode(255),
               "Error: Invalid number of arguments: 2.\n");
-
   EXPECT_EXIT(checkArgNumber(10), testing::ExitedWithCode(255),
               "Error: Invalid number of arguments: 9.\n");
-
   EXPECT_EXIT(
       {
         checkArgNumber(4);
@@ -37,21 +23,16 @@ TEST_F(ProgargsTest, checkArgNumber_Test) {
       testing::ExitedWithCode(0), "function did not exit");
 }
 
-TEST_F(ProgargsTest, parseInt_Test) {
+TEST_F(ProgargsTest, parseIntTest) {
   EXPECT_EXIT(parseInt("This won't work"), testing::ExitedWithCode(255),
               "Error: time steps must be numeric.\n");
-
   EXPECT_EXIT(parseInt(""), testing::ExitedWithCode(255), "Error: time steps must be numeric.\n");
-
   EXPECT_EXIT(parseInt("31415926535897942881273403249231843824914892318491258021753"),
               testing::ExitedWithCode(255), "Error: time steps must be numeric.\n");
-
   EXPECT_EXIT(parseInt("3.1415"), testing::ExitedWithCode(255),
               "Error: time steps must be numeric.\n");
-
   EXPECT_EXIT(parseInt("-1"), testing::ExitedWithCode(254),
               "Error: Invalid number of time steps.\n");
-
   EXPECT_EXIT(
       {
         parseInt("100");
@@ -61,8 +42,8 @@ TEST_F(ProgargsTest, parseInt_Test) {
       testing::ExitedWithCode(0), "function did not exit");
 }
 
-TEST_F(ProgargsTest, parseInputFile_Test) {
-  std::string const badInput1 = "Nothing";
+TEST_F(ProgargsTest, parseInputFileTest) {
+  std::string const badInput1 = "invalid_file";
   EXPECT_EXIT(parseInputFile(badInput1), testing::ExitedWithCode(253),
               "Error: Cannot open " + badInput1 + " for reading\n");
   EXPECT_EXIT(parseInputFile(""), testing::ExitedWithCode(253),
@@ -76,40 +57,25 @@ TEST_F(ProgargsTest, parseInputFile_Test) {
   std::string const badInput4 = "test-input/small-test-zero-particles.fld";
   EXPECT_EXIT(parseInputFile(badInput4), testing::ExitedWithCode(251),
               "Error: Invalid number of particles: 0.\n");
-
   std::string const goodInput = "reference-input/small.fld";
-  EXPECT_EXIT(
-      {
-        parseInputFile(goodInput);
-        std::cerr << "function did not exit";
-        exit(0);
-      },
-      testing::ExitedWithCode(0), "function did not exit");
+  testing::internal::CaptureStdout();
+  EXPECT_EXIT( { parseInputFile(goodInput); exit(0); }, testing::ExitedWithCode(0), "");
+  std::string const expectedOutput = "Number of particles: 4800\nParticles per meter: 204\n"
+   "Smoothing length: 0.00830882\nParticle mass: 0.00011779\nGrid size: 15 x 21 x 15\n"
+   "Number of blocks: 4725\nBlock size: 0.00866667 x 0.00857143 x 0.00866667\n";
+  EXPECT_EQ(testing::internal::GetCapturedStdout(), expectedOutput);
 }
 
-TEST_F(ProgargsTest, printGridInformation_Test) {
-  std::string const inputFile = "reference-input/small.fld";
-  EXPECT_EXIT(
-      {
-        printGridInformation(parseInputFile(inputFile).parameters);
-        std::cerr << "function did not exit";
-        exit(0);
-      },
-      testing::ExitedWithCode(0), "function did not exit");
-  inFile.open(inputFile, std::ios::binary);
-  // Will initialize basic values then also factors, which we are testing
-  parameters.initialize(inFile);
-}
-
-TEST_F(ProgargsTest, writeFile_Test) {
+TEST_F(ProgargsTest, writeFileTest) {
     std::string const goodInput = "reference-input/small.fld";
-    std::string const goodOutput = "output.fld";
+    std::string const goodOutput = "utest/output.fld";
     std::string const badOutput = "directory/";
-    const float ppm = 204;
-    const int numPart = 4800;
-    writeFile(goodOutput, ppm, numPart, parseInputFile(goodInput).part_dict);
+    float const ppm = 204;
+    int const numPart = 4800;
+    std::vector<particle> const particles = parseInputFile(goodInput).part_dict;
+    writeFile(goodOutput, ppm, numPart, particles);
     EXPECT_TRUE(compare_binary_files(goodOutput, goodInput));
-    EXPECT_EXIT(writeFile(badOutput, ppm, numPart, parseInputFile(goodInput).part_dict), testing::ExitedWithCode(252),
-                "Error: Cannot open " + badOutput + " for writing\n");
     EXPECT_EQ(remove((goodOutput).c_str()), 0);
+    EXPECT_EXIT(writeFile(badOutput, ppm, numPart, particles), testing::ExitedWithCode(252),
+                "Error: Cannot open " + badOutput + " for writing\n");
 }
